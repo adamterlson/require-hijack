39e1b47831b893cefe1f905a24a220ce06a0005b
adamterlson <adam.terlson@gmail.com>
2013-10-04 16:15:29 -0500

    Proven issue of mocking native modules via require cache

---
+++ test/require-mock.test.js
@@ -0,0 +1,17 @@
+var requiremock = require('../index');
+
+describe('require-mock', function() {
+	it('should not call original fs', function () {
+		var stub;
+
+		requiremock.replace('fs').with(function (orig) {
+			return stub = sinon.stub(orig);
+		});
+
+		var fsmodule = require('./fixture/fsmodule');
+
+		fsmodule();
+
+		stub.should.have.been.calledWithExactly('somedir')
+	});
+});
