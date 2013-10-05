dce3aa7824bc8651e7251f52bbfda750dab44b14
adamterlson <adam.terlson@gmail.com>
2013-10-05 16:43:00 -0500

    Removing function support for dep, so that the dep itself can be a function

--- test/require-mock.test.js
+++ test/require-mock.test.js
@@ -2,11 +2,9 @@ var requiremock = require('../index');
 
 describe('require-mock', function() {
 	it('should not call original fs', function () {
-		var stub;
+		var stub = { readdir: sinon.spy() };
 
-		requiremock.replace('fs').with(function (orig) {
-			return stub = { readdir: sinon.spy() };
-		});
+		requiremock.replace('fs').with(stub);
 
 		// These need to be out of the test folder because require is getting stomped, but
 		// the test runner is loading all files in the folder
