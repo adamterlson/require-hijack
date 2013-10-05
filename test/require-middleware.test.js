acb041c524c40875b51a23a8da7f5a6a2b73f3dc
adamterlson <adam.terlson@gmail.com>
2013-10-05 13:41:32 -0500

    Expressive interface for express middleware

--- test/require-middleware.test.js
+++ test/require-middleware.test.js
@@ -1,16 +1,47 @@
-var requiremiddleware = require('../lib/require-middleware');
+var requireMiddleware = require('../lib/require-middleware');
 
 describe('require-middleware', function() {
 	describe('use', function () {
+		beforeEach(function () {
+			requireMiddleware.stack = [];
+		});
+
 		it('should add middleware to the stack', function () {
 			// arrange
 			var middleware = function () { };
 
 			// act
-			requiremiddleware.use(middleware);
+			requireMiddleware.use(middleware);
 
 			// assert
-			expect(requiremiddleware.stack.indexOf(middleware)).to.equal(0);
+			expect(requireMiddleware.stack[0].handle).to.equal(middleware);
+		});
+
+		it('should name middleware based on function name by default', function () {
+			// arrange
+			var middleware = function myname() { };
+
+			// act
+			requireMiddleware.use(middleware);
+
+			// assert
+			console.log(requireMiddleware.stack);
+			expect(requireMiddleware.stack[0].handle).to.equal(middleware);
+			expect(requireMiddleware.stack[0].name).to.equal('myname');
+		});
+
+		describe('as', function () {
+			it('should change the middlewares name', function () {
+				// arrange
+				var middleware = function myname() { };
+
+				// act
+				requireMiddleware.use(middleware).as('newname');
+
+				// assert
+				expect(requireMiddleware.stack[0].handle).to.equal(middleware);
+				expect(requireMiddleware.stack[0].name).to.equal('newname');
+			});
 		});
 	});
 });
